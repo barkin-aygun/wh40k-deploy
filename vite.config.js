83fe3bc9eca6105cceb861eed3907c8fa1eb8bf0
Barkin Aygun <barkin@fastmail.com>
2026-01-26 20:29:07 -0800

    Configs

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "https://barkin-aygun.github.io/wh40k-deploy/",
-  build: {
-    sourcemap: true, // Generates production source maps
-  },
+  base: "https://barkin-aygun.github.io/wh40k-deploy/"
 })
